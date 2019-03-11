0ec1588f055380f03e6108179bd7ce99fda1e49f
Lauren <laurenribeiro06@gmail.com>
2019-03-11 04:23:28 +0000

    Updated vampire.js to include tree transversal functions

--- vampire.js
+++ vampire.js
@@ -10,22 +10,72 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    let currentVampire = this;
+    if (vampire.numberOfVampiresFromOriginal > currentVampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    let result = {};
+    if (this.name === name) {
+      result = this
+    } else {
+      for(vampire in this.offspring) {
+        if (vampire.vampire.name === name){
+          result = vampire.vampire;
+        }
+      }
+    }
+    return result;
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let totalVampires = 0;
+    for (vampire in this.offspring) {
+      let vampireCount = vampire.totalDescendents +1
+      totalVampires += vampireCount
+    }
+    return totalVampires;
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let vampires = [];
+    if(this.yearConverted > 1980) {
+      vampires.append(this)
+    }
+    for (vampire in this.offspring) {
+      let millennialVampires = vampire.allMillennialVampires
+      vampires ++ millennialVampires
+    }
+    return vampires;
   }
 
   /** Stretch **/
